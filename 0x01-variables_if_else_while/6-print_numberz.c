11bbcf063c2daf2926ed189419fbf33f4f18bee9
martinkieti <martinmunguti63@gmail.com>
2022-12-12 07:30:56 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include  <stdlib.h>
+/**
+ * main - printing out numbers using putchar
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+	/* */
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
