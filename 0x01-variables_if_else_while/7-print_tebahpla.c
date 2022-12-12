446084281a32d3e9bb608f8e80fbbb11200b73bd
martinkieti <martinmunguti63@gmail.com>
2022-12-12 07:42:29 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - printing the alphabet backward
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char x;
+	/* */
+	for (x = 'z'; x <= 'a'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
