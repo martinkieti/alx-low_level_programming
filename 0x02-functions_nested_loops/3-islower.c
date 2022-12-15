b32c23f936534728237e0f502149cf21bc8400fd
martinkieti <martinmunguti63@gmail.com>
2022-12-15 09:15:22 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase letters
+ * @c: parameter
+ * Return: 0 (Success)
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar ('\n');
+}
